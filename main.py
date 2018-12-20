0a235c941ecc2f94ae30057adcfe03848d79ee91
Luka Novak <lnovak007@cee.ema.ad.pwcinternal.com>
2018-12-20 14:25:31 +0100

    Add SVM model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 from util import *
 
@@ -7,8 +8,9 @@ from util import *
 if __name__ == '__main__':
 
     ap = argparse.ArgumentParser()
+    ap.add_argument('-m', '--model', choices=['logistic', 'svm'], required=True, help='Specify which model to use')
     ap.add_argument('-v','--verbose', required=False, action='store_true',
-        help='show image')
+        help='Verbose output')
     args = vars(ap.parse_args())
 
     reviews_train, reviews_test = load_files()
@@ -30,32 +32,50 @@ if __name__ == '__main__':
 
     target = [1 if i < 12500 else 0 for i in range(25000)]
 
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, target, train_size = 0.75
-    )
+    X_train, X_val, y_train, y_val = train_test_split(X, target, train_size = 0.75)
 
     if args['verbose']:
         print('\nX_train: {0}, y_train: {2}, X_val: {1}, y_val: {3}'.format(X_train.shape[0], X_val.shape[0], len(y_train), len(y_val)))
 
-    best, scores = get_best_param(X_train, y_train, X_val, y_val)
+    if args['model'] == 'logistic':
+        best, scores = get_best_param_logistic(X_train, y_train, X_val, y_val)
 
-    for c, s in scores:
-        print("\nAccuracy for C={}: {}".format(c, s))
+        if args['verbose']:
+            for c, s in scores:
+                print("\nAccuracy for C={}: {}".format(c, s))
 
-    best_param, best_score = best
+        best_param, best_score = best
 
-    if args['verbose']:
-        print('\nThe best accuracy of {} is for parameter c = {}'.format(best_score, best_param))
+        if args['verbose']:
+            print('\nThe best accuracy of {} is for parameter c = {}'.format(best_score, best_param))
 
-    model = train_model(X, target, X_test, best_param)
+    if args['model'] == 'svm':
+        best, scores = get_best_param_svm(X_train, y_train, X_val, y_val)
 
-    feature_to_coef = get_feat_names(cv, model)
+        if args['verbose']:
+            for c, s in scores:
+                print("\nAccuracy for C={}, kernel={}: {}".format(c[0], c[1], s))
 
-    if args['verbose']:
-        print('\nBest positive words:')
-        for best_positive in sorted(feature_to_coef.items(), reverse=True, key=lambda x: x[1])[:5]:
-            print (best_positive)
+        best_param, best_score = best
 
-        print('\nBest negative words:')
-        for best_negative in sorted(feature_to_coef.items(), key=lambda x: x[1])[:5]:
-            print (best_negative)
+        if args['verbose']:
+            print('\nThe best accuracy of {} is for parameter c = {} and kernel = {}'.format(best_score, best_param[0], best_param[1]))
+
+
+    print('\nTraining the final model...\n')
+    if args['model'] == 'logistic':
+        model = train_logistic_reg(X, target, X_test, best_param)
+    if args['model'] == 'svm':
+        model = train_svm(X, target, X_test, C=best_param[0], kernel=best_param[1])
+
+    if args['model'] == 'logistic':
+        feature_to_coef = get_feat_names(cv, model)
+
+        if args['verbose']:
+            print('\nBest positive words:')
+            for best_positive in sorted(feature_to_coef.items(), reverse=True, key=lambda x: x[1])[:5]:
+                print (best_positive)
+
+            print('\nBest negative words:')
+            for best_negative in sorted(feature_to_coef.items(), key=lambda x: x[1])[:5]:
+                print (best_negative)
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import re
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 
 def load_files():
@@ -26,7 +27,7 @@ def preprocess_reviews(reviews):
     return reviews
 
 
-def get_best_param(X_train, y_train, X_val, y_val):
+def get_best_param_logistic(X_train, y_train, X_val, y_val):
 
     scores = []
 
@@ -40,11 +41,33 @@ def get_best_param(X_train, y_train, X_val, y_val):
     return (sorted_scores[0], scores)
 
 
-def train_model(X, target, X_test, c):
+def train_logistic_reg(X, target, X_test, c):
     final_model = LogisticRegression(C=c)
     final_model.fit(X, target)
-    print ("Final Accuracy: %s"
-        % accuracy_score(target, final_model.predict(X_test)))
+    print ("Final Accuracy: {}".format(accuracy_score(target, final_model.predict(X_test))))
+
+    return final_model
+
+
+def get_best_param_svm(X_train, y_train, X_val, y_val):
+
+    scores = []
+
+    for c in [0.05, 0.25, 0.5, 1]:
+        for kernel in ['linear', 'poly', 'rbf', 'sigmoid']:
+
+            lr = SVC(C=c, kernel=kernel, gamma='scale')
+            lr.fit(X_train, y_train)
+            scores.append(((c, kernel), accuracy_score(y_val, lr.predict(X_val))))
+
+    sorted_scores = sorted(scores, reverse=True, key=lambda a: a[1])
+    return (sorted_scores[0], scores)
+
+
+def train_svm(X, target, X_test, C, kernel):
+    final_model = SVC(C=C, kernel=kernel, gamma='scale')
+    final_model.fit(X, target)
+    print ("Final Accuracy: {}".format(accuracy_score(target, final_model.predict(X_test))))
 
     return final_model
 
